12df90eeaa0369fba8e0af4732a10bb822a4ea2b
Arya-H03 <Arya-H03@users.noreply.github.com>
2025-02-03 13:07:17 -0500

    Add file reading and text

--- Bouncing Shapes/Bouncing Shapes/main.cpp
+++ Bouncing Shapes/Bouncing Shapes/main.cpp
@@ -5,37 +5,69 @@
 #include <iostream>
 #include <fstream>
 
+const std::string configPath = "./resources/config.txt";
+
+
+unsigned int wWidth = 100;
+unsigned int wHeight = 100;
+
+
+std::string fontPath = "";
+sf::Font font;
+int fontSize = 14;
+int fontColor[3] = { 0,0,0 };
+
+
+
+
+
+
 class MyShape
 {
-    sf::Shape* m_shape = nullptr;
+    
+    std::shared_ptr <sf::Shape> m_shape;
     std::string m_name;
+    std::string m_shapeType;
     std::array<float, 2> m_size;       // {width, height}
-    std::array<float, 2> m_speed; // {xSpeed, ySpeed}
-    std::array<float, 2> m_pos;   // {xPos, yPos}
-    std::array<int, 3> m_color;      // {r, g, b}
+    std::array<float, 2> m_speed;      // {xSpeed, ySpeed}
+    std::array<float, 2> m_pos;        // {xPos, yPos}
+    std::array<int, 3> m_color;        // {r, g, b}
+
+    sf::Text m_text;
 
 public:
 
-    MyShape(std::string name, std::array<float, 2> size, std::array<float, 2> startSpeed, std::array<float, 2> startPos, std::array<int, 3> color)
-
-        : m_name(name), m_size(size), m_speed(startSpeed), m_pos(startPos), m_color(color)
-     {
-        if (m_name == "Circle")
+    MyShape(std::string name, std::string shapeType, std::array<float, 2> size, std::array<float, 2> startSpeed, std::array<float, 2> startPos, std::array<int, 3> color)
+        : m_name(name), m_shapeType(shapeType), m_size(size), m_speed(startSpeed), m_pos(startPos), m_color(color), m_text(font, m_name, fontSize)
+    {
+        if (m_shapeType == "Circle")
         {
-            m_shape = new sf::CircleShape(m_size[0],32);
+            m_shape = std::make_shared<sf::CircleShape>(m_size[0], 32); // Use shared_ptr
+            std::cout << "Circle was created with name " << m_name << std::endl;
         }
-        else if (m_name == "Rectangle")
+        else if (m_shapeType == "Rectangle")
         {
-            m_shape = new sf::RectangleShape({size[0],size[1]});
+            m_shape = std::make_shared<sf::RectangleShape>(sf::Vector2f(m_size[0], m_size[1])); // Use shared_ptr
+            std::cout << "Rect was created with name " << m_name << std::endl;
         }
         else
         {
             m_shape = nullptr;
+            std::cout << "Shape was not created" << std::endl;
         }
 
-     }
+        ChangeColor(m_color);
+        // Initialize m_text after the font is loaded
+        m_text.setFont(font);
+        m_text.setString(m_name);
+        m_text.setCharacterSize(fontSize);
+        m_text.setFillColor(sf::Color(fontColor[0], fontColor[1], fontColor[2]));
+    }
 
-     sf::Shape* GetShape() const
+    // Other methods...
+
+
+     std::shared_ptr <sf::Shape> GetShape() const
      {
          if (m_shape != nullptr)
          {
@@ -74,6 +106,17 @@ public:
          return m_name;
      }
 
+     sf::Text* GetText()
+     {
+         return &m_text;
+     }
+
+     void ChangeName(std::string newName)
+     {
+         m_name = newName;
+         m_text.setString(m_name);
+     }
+
      void ChangeSize(std::array<float, 2> newSize)
      {
          m_size = newSize;
@@ -94,7 +137,7 @@ public:
      void ChangeColor(std::array<int, 3> newColor)
      {
          m_color = newColor;
-         m_shape->setFillColor(sf::Color(std::uint8_t(m_color[0] * 255), std::uint8_t(m_color[1] * 255), std::uint8_t(newColor[2] * 255)));
+         m_shape->setFillColor(sf::Color(std::uint8_t(m_color[0]), std::uint8_t(m_color[1] ), std::uint8_t(newColor[2] )));
      }
 
      void MoveShape()
@@ -102,44 +145,121 @@ public:
          m_pos[0] += m_speed[0];
          m_pos[1] += m_speed[1];
          m_shape->setPosition({ m_pos[0], m_pos[1] });
-     }
+
+         m_text.setPosition({ m_shape->getGlobalBounds().getCenter().x - m_text.getGlobalBounds().size.x / 2,m_shape->getGlobalBounds().getCenter().y - m_text.getGlobalBounds().size.y });
+     }  
 
      void CheckForBounce(sf::RenderWindow *window, float* xSpeed, float* ySpeed)
      {
          if (m_shape->getGlobalBounds().getCenter().y - m_shape->getGlobalBounds().size.y / 2 <= 0)
         {
-            *ySpeed *= -1;
+            m_speed[1] *= -1;
         }
         if (m_shape->getGlobalBounds().getCenter().y + m_shape->getGlobalBounds().size.y / 2 >= window->getSize().y)
         {
-            *ySpeed *= -1;
+            m_speed[1] *= -1;
         }
 
         if (m_shape->getGlobalBounds().getCenter().x - m_shape->getGlobalBounds().size.x / 2 <= 0)
         {
-            *xSpeed *= -1;
+            m_speed[0] *= -1;
         }
         if (m_shape->getGlobalBounds().getCenter().x+ m_shape->getGlobalBounds().size.x / 2 >= window->getSize().x)
         {
-            *xSpeed *= -1;
+            m_speed[0] *= -1;
         }
      }
 
-     ~MyShape()
-     {
-         delete m_shape; // Prevent memory leak
-     }
+     //~MyShape()
+     //{
+     //    if (m_shape != nullptr)
+     //    {
+     //        delete m_shape;
+     //        m_shape = nullptr; // Prevent double delete
+     //    }
+     //}
 
 };
 
+void ReadFromFile(const std::string& filename, std::vector<MyShape> *shapes)
+{
+    std::string temp;
+    std::ifstream fin(filename);
 
+    std::string shapeName;
+    std::array<float, 2> pos;
+    std::array<float, 2> speed;
+    std::array<float, 2> size;
+    std::array<int, 3> color;
+    float radius;
+
+
+    while (fin >> temp)
+    {
+        if (temp == "Window") {
+            if (!(fin >> wWidth >> wHeight)) {
+                std::cerr << "Error: Invalid format for window size." << std::endl;
+                return;
+                
+            }
+        }
+        else if (temp == "Font")
+        {
+            if (!(fin >> fontPath >> fontSize >> fontColor[0] >> fontColor[1] >> fontColor[2]))
+            {
+                std::cerr << "Error: Invalid format for font.";
+                return;
+            }
+        }
+        else if (temp == "Circle")
+        {
+            if (!(fin >> shapeName >> pos[0] >> pos[1] >> speed[0] >> speed[1] >> color[0] >> color[1] >> color[2] >> radius))
+            {
+                std::cerr << "Error: Invalid format for Circle.";
+                return;
+
+
+            }
+            else
+            {
+
+                shapes->push_back(MyShape(shapeName, temp, { radius, radius }, speed, pos, color));
+            }
+        }
+        else if(temp == "Rectangle")
+        {
+            if (!(fin >> shapeName >> pos[0] >> pos[1] >> speed[0] >> speed[1] >> color[0] >> color[1] >> color[2] >> size[0]>>size[1]))
+            {
+                std::cerr << "Error: Invalid format for Rectangle.";
+                return;
+
+
+            }
+            else
+            {
+
+                shapes->push_back(MyShape(shapeName, temp, { size[0], size[1] }, speed, pos, color));
+            }
+        }
+
+    }
+}
 int main()
 {
-    const int wWidth = 1280;
-    const int wHeight= 720;
-    sf::RenderWindow window(sf::VideoMode({ wWidth, wHeight }), "SFML works!");
-    window.setFramerateLimit(60);
+    std::vector < MyShape> shapes;
 
+    ReadFromFile(configPath, &shapes);
+
+    /*std::cerr << "Name: " << shapes[4].GetName() << "\n"
+        << "Position: (" << shapes[4].GetPosition()[0] << ", " << shapes[4].GetPosition()[1] << ")\n"
+        << "Speed: (" << shapes[4].GetSpeed()[0] << ", " << shapes[4].GetSpeed()[1] << ")\n"
+        << "Color: (" << shapes[4].GetColor()[0] << ", " << shapes[4].GetColor()[1] << ", " << shapes[4].GetColor()[2] << ")\n"
+        << "Size: " << shapes[4].GetSize()[0] << shapes[4].GetSize()[1]<< "\n"
+        << "Shape Pointer: " << shapes[4].GetShape() << std::endl;*/
+
+    sf::RenderWindow window(sf::VideoMode({ wWidth, wHeight }), "Bounding Shapes!");
+    window.setFramerateLimit(60);
+    font.openFromFile(fontPath);
 
     ImGui::SFML::Init(window);
     sf::Clock deltaclock;
@@ -148,30 +268,12 @@ int main()
     ImGui::GetIO().FontGlobalScale = 2.0f;
 
     float c[3] = { 0.0f,1.0f,1.0f };
-
     float radius = 50;
     int segments = 32;
     float xSpeed = 1.0f;
     float ySpeed = 0.5f;
     bool drawCircle = true;
     bool drawText = true;
-
- 
-
-    MyShape shape("Circle", { 100, 100 }, { 1, 1 }, { 50, 50 }, { 1, 0, 0 }); // Red rectangle
-
-       /* const sf::Font font("arial.ttf");*/
-
-
-    /*if (!font.openFromFile("file"))
-    {
-        std::cerr << "Font not found \n";
-        exit(-1);
-
-    }*/
-
-    /*sf::Text text(font,"Sampletext", 24);*/
-    
     
 
     while (window.isOpen())
@@ -197,39 +299,29 @@ int main()
         ImGui::Checkbox("Draw Text", &drawText);
         ImGui::SliderFloat("Radius", &radius, 0.0f, 100.0f);
         ImGui::SliderInt("Sides", &segments, 3, 64);
-        ImGui::SliderFloat("Xspeed", &xSpeed, -100.0f, 100.0f);
-        ImGui::SliderFloat("Yspeed", &ySpeed, -100.0f, 100.0f);
+        ImGui::SliderFloat("Xspeed", &xSpeed, -10.0f, 10.0f);
+
+        ImGui::SliderFloat("Yspeed", &ySpeed, -10.0f, 10.0f);
         ImGui::ColorEdit3("Color", c);
         //ImGui::InputText("Text", displayString, 255);
         ImGui::End();
 
-        shape.ChangeSpeed({ xSpeed,ySpeed });
-
-        shape.ChangeColor({ 0,1,1 });
-
-        shape.MoveShape();
-
-
-       /* text.setStyle(sf::Text::Bold);
-        text.setFillColor(sf::Color::Red);
-        text.setPosition({ 100.0f,100.0f });*/
-
-        
-
-        shape.CheckForBounce(&window,&xSpeed,&ySpeed);
-        
+        for (MyShape& shape : shapes)
+        {
+            shape.MoveShape();
+            shape.CheckForBounce(&window, &xSpeed, &ySpeed);
+        }
         
 
         window.clear();
-        //if (drawCircle)
-        //{
-        //   /* window.draw(text);*/
-        //    window.draw(circle);
-        //}
-
-        window.draw(*shape.GetShape());
-       
         
+        for (MyShape& shape : shapes)
+        {
+            window.draw(*shape.GetShape());
+            window.draw(*shape.GetText());
+        }
+
+    
         ImGui::SFML::Render(window);
         window.display();
     }
